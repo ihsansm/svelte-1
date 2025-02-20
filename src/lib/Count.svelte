c200681b3357df2140ff0275601b293563ac5072
ihsan <ihsantwf301@gmail.com>
2025-02-20 20:23:02 +0700

    feat: adding + / - button for count counter

---
+++ src/lib/Count.svelte
@@ -0,0 +1,19 @@
+<script>
+    let count = $state(0)
+    const increment = () => {
+        count += 1
+    }
+    const decrement = () => {
+        count -= 1
+    }
+</script>
+
+<h1>
+    count : {count}
+    <br>
+
+    <br>
+    
+    <button onclick={increment}>+</button>
+    <button onclick={decrement}>-</button>
+</h1>
