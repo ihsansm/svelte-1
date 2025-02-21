98c2dc52750775167afaa0fddf52a737714ae530
ihsan <ihsantwf301@gmail.com>
2025-02-21 19:49:03 +0700

    feat: implementing if-else on counter text

--- src/lib/Count.svelte
+++ src/lib/Count.svelte
@@ -16,6 +16,21 @@
 
     <br>
 
-    <button onclick={increment} id="i-button">+</button>
-    <button onclick={decrement} id="d-button">-</button>
+    <button onclick={increment} 
+    id="i-button">
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+    </button>
+    <button onclick={decrement} 
+    id="d-button">
+    -
+    </button>
+
+    {#if count > 10}
+        <p>{count} is greater than 10</p>
+    {:else if count > 5}
+        <p>{count} is greater than 5</p>
+    {:else}
+        <p>{count} is between 5 and 10</p>
+    {/if}
 </h1>
